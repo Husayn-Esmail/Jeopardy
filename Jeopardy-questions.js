4f9148176a277fdd8c9fe6eb3037559d2129a868
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2020-02-20 06:58:40 -0700

    added files

---
+++ Jeopardy-questions.js
@@ -0,0 +1,7 @@
+var seconds = document.getElementById("countdown").textContent;
+var countdown = setInterval(function(){
+    seconds--;
+    (seconds == 1) ? document.getElementById("plural").textContent = "" : document.getElementById("plural").textContent = "s";
+    document.getElementById("countdown").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+},1000);
