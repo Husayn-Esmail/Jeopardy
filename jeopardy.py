feb7944f7a1128d170cd1c0608426772d1e711de
Husayn <husayn@esmail.ca>
2020-03-08 15:21:32 -0600

    added file wrote more python

--- jeopardy.py
+++ jeopardy.py
@@ -1,16 +1,33 @@
 
 class Question:
-    def __init__(self,QuestionID, CatID, QuestionTxt, AnswerTxt, PointVal):
+    def __init__(self,QuestionID, QuestionTxt, AnswerTxt, PointVal):
         self.QuestionID= QuestionID
-        self.CatID= CatID
         self.QuestionTxt=QuestionTxt
         self.AnswerTxt=AnswerTxt
         self.PointVal=PointVal
-        print("question is:" + self.QuestionTxt)
+    def prnt_all(self):
+        print(self.QuestionID)
+        print(self.QuestionTxt)
+        print(self.AnswerTxt)
+        print(self.PointVal)
 
 
 
 class Category:
     def __init__(self, category):
         self.category=category
+        self.Questions=[]
 
+class Game:
+    def __init__(self):
+        self.Categories=[]
+
+Jeopardy=Game()
+
+with open("Book1.csv", "r") as filestream:
+    for line in filestream:
+        currentline= line.split(",")
+        q1=Question(Category, QuestionID, Question, Answer, Points)
+
+
+#Jeopardy=
