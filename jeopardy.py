3df131ce4a7f41ff25ed0462eb811817164d18af
Husayn <husayn@esmail.ca>
2020-03-08 14:08:50 -0600

    created a py file

---
+++ jeopardy.py
@@ -0,0 +1,21 @@
+catID=[]
+ID=[
+
+]
+question={
+
+}
+
+
+class Question:
+    QuestionID=-1
+    CatID=-1
+    QuestionTxt=""
+    AnswerTxt=""
+    PointVal=-1
+
+
+class Category:
+    category={
+        1:"", 2:"", 3:"", 4:"", 5:""
+    }
