0cbfc8b5abb276d9304f2d87d8bb1fde75797354
Husayn Esmail <husayn@esmail.ca>
2020-03-10 03:20:04 +0000

    FIXED PRINTING PYTHON!!

--- jeopardy.py
+++ jeopardy.py
@@ -5,10 +5,8 @@ class Question:
         self.QuestionTxt= QuestionTxt
         self.AnswerTxt= AnswerTxt
         self.PointVal= PointVal
-    def __repr__(self):
-         return self.PointVal, self.QuestionID, self.AnswerTxt, self.PointVal
-
-
+    def __str__(self):
+      return '{} {} {} {}'.format(self.QuestionID, self.QuestionTxt, self.AnswerTxt, self.PointVal)
 
 class Category:
     def __init__(self, category):
@@ -24,8 +22,8 @@ Jeopardy=Game()
 with open("Book1.csv", "r") as filestream:
     for line in filestream:
         currentline= line.split(",")
-        q1=Question(currentline[1], currentline[0], currentline[2], currentline[3])
-        print(currentline)
+        q1=Question(currentline[0], currentline[3], currentline[1], currentline[2])
+        print(q1)
 
 
 #Jeopardy=
