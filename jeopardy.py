78a696e30e5298bbd7e684b0317547b243efb190
Husayn <husayn@esmail.ca>
2020-03-10 18:46:25 -0600

    changed some stuff

--- jeopardy.py
+++ jeopardy.py
@@ -5,10 +5,10 @@ print("<head>")
 print("<link rel='stylesheet' href='/Jeopardy/styles.css'>")
 print("</head>")
 print("<body>")
-print("<div class = "center" >")
+print("<div class=center>")
 #This is the Jeopardy font. It's an image, if you ever have to change it, go to this website to use the same font https://fontmeme.com/jeopardy-font/-->
-print("<a href="https://fontmeme.com/jeopardy-font/">")
-print("<img src="https://fontmeme.com/permalink/200220/6b98c047956a0573caad64c61c62769d.png" alt="jeopardy-font" border="0" id="jeop">")
+print("<a href=https://fontmeme.com/jeopardy-font/>")
+print("<img src=https://fontmeme.com/permalink/200220/6b98c047956a0573caad64c61c62769d.png alt=jeopardy-font border=0 id=jeop>")
 print("</a>")
 print("</div>")
 
@@ -27,12 +27,14 @@ class Category:
     def __init__(self, category):
         self.category=category
         self.Questions=[]
-    #def __str__(self):
-    #   return '{} {}'.format(self.category, self.Questions)
+    def __str__(self):
+       return '{} {}'.format(self.category, self.Questions)
 
 class Game:
     def __init__(self):
         self.Categories = []
+    def __str__(self):
+        return '{}'.format(self.Categories)
 #creating game
 Jeopardy=Game()
 
@@ -44,39 +46,54 @@ nbrCat = -2
 print ("<table>")
 
 with open("Book1.csv", "r") as filestream:
+    questionsDict = {}
     for line in filestream:
         if nbrCat == -2:
             nbrCat += 1
         else:
-            currentline= line.split(",")
-            cat = currentline[0]
-            if cat != oldcat:
-                Jeopardy.Categories.append(Category(cat))
-                oldcat = cat
-                nbrCat = nbrCat + 1
-            qid = currentline[1]
-            qtxt = currentline[2]
-            atxt = currentline[3]
-            pval = currentline[4]
+            currentLine=line.split(",")
+            currentQuestion=[currentLine[1],currentLine[2],currentLine[3],currentLine[4].rstrip()]
+            questionsDict.update({currentLine[0] : currentQuestion })
+            print(questionsDict)
+            # if cat != oldcat:
+            #     Jeopardy.Categories.append(Category(cat))
+            #     oldcat = cat
+            #     nbrCat = nbrCat + 1
+            # qid = currentline[1]
+            # qtxt = currentline[2]
+            # atxt = currentline[3]
+            # pval = currentline[4]
 
-            Jeopardy.Categories[nbrCat].Questions.append(Question(qid,qtxt,atxt,pval))
+            #Jeopardy.Categories[nbrCat].Questions.append(Question(qid,qtxt,atxt,pval))
+            for x in range(len(Jeopardy.Categories)):
+                for y in range(len(Jeopardy.Categories[x].Questions)):
+                    print(Jeopardy.Categories[x].Questions[y])
+            
+                
+            #print(str(Jeopardy.Categories[0].Questions))
+            
 #adds a table row and prints a header per each category
 print("<tr>")
 for c in Jeopardy.Categories:
     print("<th>")
     print(c.category)
     print("</th>")
+#if cat != oldcat:
+#    ind=-1
+#    for x in Jeopardy.Categories:        
+#        ind += 1
+#        print("<button class=but>")
+#        print(cat.Questions[ind])
 print("</tr>")
 #CHANGE TO QUESTIONS and increment
 print("<tr>")
-for c in Jeopardy.Categories:
-    print("<th>")
-    print(c.category)
-    print("</th>")
-print("</tr>")
 
+
+
+
+print("</tr>")
 print ("</table>")
 
 print("</body>")
 print("</html>")
-#Jeopardy=
+
